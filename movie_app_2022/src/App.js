f15fe75b2540a36c3886fdb433db3086f9c34c0e
5nam <ohnam90@gmail.com>
2022-03-29 23:19:20 +0900

    03 리액트 기초 개념

--- movie_app_2022/src/App.js
+++ movie_app_2022/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}){ 
+  // App.js 파일 안에서 컴포넌트 만들고, Potato 컴포넌트를 App 컴포넌트 안에서 사용함.
+  // 이렇게 컴포넌트 안에 컴포넌트를 포함시키는 이유는 파일 이동하면서 코드 작업하기가 불편하기 때문.
+  return <h1>I like { fav }</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
+  </div>);
 }
 
 export default App;
